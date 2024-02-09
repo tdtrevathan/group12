dd7f26ee5e3debcd084c56d8acfbab18581e2e5c
Josh <90493234+JoshEddie@users.noreply.github.com>
2024-02-09 12:21:00 -0600

    React initial setup

---
+++ front-end/src/components/FormFields.jsx
@@ -0,0 +1,31 @@
+function Label () {
+    return (
+        <>
+        <label for=""></label>{/* TODO */}
+        </>
+    )
+}
+
+export function Button ( { buttonText } ) {
+    return (
+        <>
+        {/* TODO */}
+        </>
+    )
+}
+
+export function Radio ( {label, } ) {
+    return (
+        <>
+        {/* TODO */}
+        </>
+    )
+}
+
+export function TextField ( {label, type } ) {
+    return (
+        <>
+        {/* TODO */}
+        </>
+    )
+}
---
+++ front-end/src/components/FuelQuote.jsx
@@ -0,0 +1,7 @@
+export default function FuelQuote () {
+    return (
+        <>
+        {/* TODO */}
+        </>
+    )
+}
