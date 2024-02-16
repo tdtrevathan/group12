188f0c39272a45b781638f0040eabdc7f46f6d4c
Josh <90493234+JoshEddie@users.noreply.github.com>
2024-02-16 12:00:27 -0600

    Added basic nav for testing pages

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,5 +1,15 @@
+import '../css/nav.css'
+
 export default function Nav () {
     return (
-        <nav></nav> /* TODO */
+        <nav id="mainNav">
+
+            <a href="/">Login</a>
+            <a href="/register">Register</a>
+            <a href="/profile">Profile</a>
+            <a href="/fuelquote">Fuel Quote</a>
+            <a href="/quotehistory">Quote History</a>
+
+        </nav> /* TODO nav only shows after login, login and register should be removed at certain point*/
     )
 }
