901b2d6d6d3dfcfe0c4aec4a3b1d4e4cfb332825
kaylagoldsmith <kaylagold97@gmail.com>
2024-02-17 16:12:49 -0600

    This is Kayla's commit for registration

--- front-end/src/components/Registration.jsx
+++ front-end/src/components/Registration.jsx
@@ -1,6 +1,12 @@
 export default function Registration () {
     return (
         <>
+        <form>
+            <Input name="firstname" label="First Name:" type="text"></Input>
+            <Input name="lastName" label="Last Name:" type="text"></Input>
+            <Input name="username" label="Username:" type="text"></Input>
+            <Input name="password" label="Password:" type="password"></Input>
+        </form>
         {/* TODO */}
         </>
     )
