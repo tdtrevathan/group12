7ed421d52b42f0e1402b0946684874bad39c6f5f
Josh <90493234+JoshEddie@users.noreply.github.com>
2024-02-20 20:57:53 -0600

    adding shownav usestate

--- front-end/src/components/Nav.jsx
+++ front-end/src/components/Nav.jsx
@@ -1,14 +1,20 @@
 import '../css/nav.css'
 
-export default function Nav () {
+export default function Nav ( {navShow} ) {
     return (
         <nav id="mainNav">
 
-            <a href="/">Login</a>
-            <a href="/register">Register</a>
-            <a href="/profile">Profile</a>
-            <a href="/fuelquote">Fuel Quote</a>
-            <a href="/quotehistory">Quote History</a>
+            {navShow ?
+                <>
+                <a href="/">Login</a>
+                <a href="/register">Register</a>
+                <a href="/profile">Profile</a>
+                <a href="/fuelquote">Fuel Quote</a>
+                <a href="/quotehistory">Quote History</a>
+                </>
+                :
+                ''
+            }
 
         </nav> /* TODO nav only shows after login, login and register should be removed at certain point*/
     )
