691fb40006c99e345bceeda5ee6514c61b35c29e
tdtrevathan <trevathantd@gmail.com>
2024-03-07 16:56:53 -0600

    implemented mock

--- project/src/test/java/group12/project/ProfileRepoTests.java
+++ project/src/test/java/group12/project/ProfileRepoTests.java
@@ -2,22 +2,34 @@ package group12.project;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+import group12.project.Models.profileModel;
 import group12.project.Views.profileView;
+
+import java.util.Optional;
 
 @SpringBootTest
 @ExtendWith(MockitoExtension.class)
 class ProfileRepoTests {
 
-	ProfileRepo profileRepo = new ProfileRepo();
+	@InjectMocks
+	ProfileRepo profileRepo;
+
+    @Mock
+    private profileModel repo;
 
 	@Test
-	public void createProfile_ShouldCreateProfile(){
+	public void getProfile_ShouldReturnProfile(){
 
 		profileView profile = new profileView(
             "1",
@@ -28,11 +40,11 @@ class ProfileRepoTests {
             "TX", 
             "77336");
 
-		profileRepo.create(profile);
-		var retrievedProfile = profileRepo.get(profile.getId());
+		Mockito.when(repo.findById(profile.getId()))
+			.thenReturn(Optional.of(profile));
 
-		assertEquals(profile, retrievedProfile);
+		var result = profileRepo.get(profile.getId());
 
-		profileRepo.delete(profile.getId());
+		assertEquals(profile, result);
 	}
 }
