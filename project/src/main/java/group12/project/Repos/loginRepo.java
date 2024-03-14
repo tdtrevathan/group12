a428a6798cdcc652979f56fc3a8a6aa9f6242511
Josh <90493234+JoshEddie@users.noreply.github.com>
2024-03-14 08:06:56 -0500

    Added login and fuelquote repo files (copied from profileRepo)

---
+++ project/src/main/java/group12/project/Repos/loginRepo.java
@@ -0,0 +1,11 @@
+package group12.project.Repos;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import group12.project.Views.profileView;
+
+@Repository
+public interface loginRepo extends MongoRepository<profileView, String> {
+    
+}
