e8df92ccb7cd1d3d751c914d8c781cf18261e1f4
kaylagoldsmith <kaylagold97@gmail.com>
2024-03-28 12:55:21 -0500

    Update fuelQuoteView.java

--- project/src/main/java/group12/project/Views/fuelQuoteView.java
+++ project/src/main/java/group12/project/Views/fuelQuoteView.java
@@ -12,12 +12,33 @@ public class fuelQuoteView {
         String username,
         String gallons,
         String address,
+<<<<<<< Updated upstream
         String date,
         String rate,
         String total
     )
     {
         this.username = username;
+=======
+        Date date,
+        double rate,
+        double total,
+        String userId
+    ) throws Exception
+    {
+        if(isGallonsValid(gallons)){
+            this.gallons = gallons;
+        }
+        else{
+            throw new Exception(gallons + "was not valid");
+        }
+        if(isDateValid(date)){
+            this.date = date;
+        }
+        else{
+            throw new Exception(date + "was not valid");
+        }
+>>>>>>> Stashed changes
         this.gallons = gallons;
         this.address = address;
         this.date = date;
@@ -28,7 +49,23 @@ public class fuelQuoteView {
     private String username;
     private String gallons;
     private String address;
+<<<<<<< Updated upstream
     private String date;
     private String rate;
     private String total;
+=======
+    private Date date;
+    private double rate;
+    private double total;
+    private String userId;
+
+    private boolean isGallonsValid(double inputGallons){
+        if(inputGallons <=0 || inputGallons == 0) return false;
+        return true;
+    }
+    private boolean isDateValid(date inputDate){
+        if(!inputDate) return false;
+        return true;
+    }
+>>>>>>> Stashed changes
 }
