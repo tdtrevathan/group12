5305b50cc48d7d6a586c615ba801d5998c4d4157
Josh <90493234+JoshEddie@users.noreply.github.com>
2024-03-29 14:16:24 -0500

    Cleaned yup imports and verified tests of Pricing Module test

--- project/src/test/java/group12/project/PricingModuleTests.java
+++ project/src/test/java/group12/project/PricingModuleTests.java
@@ -1,12 +1,10 @@
 package group12.project;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import group12.project.PricingModule;
 import group12.project.Views.fuelQuoteView;
 
 @SpringBootTest
